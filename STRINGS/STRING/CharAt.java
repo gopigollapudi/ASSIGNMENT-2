3b0b7d2b8def25fc6c2f2976253239970acf62c4
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:08:32 +0530

    CharAt

---
+++ STRINGS/STRING/CharAt.java
@@ -0,0 +1,8 @@
+package STRING;
+
+public class CharAt{
+    public static void main(String args[]){
+        String name="javatpoint";
+        char ch=name.charAt(4);//returns the char value at the 4th index
+        System.out.println(ch);
+    }}
