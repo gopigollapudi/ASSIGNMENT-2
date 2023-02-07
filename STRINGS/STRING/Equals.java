db686bf4c45591e8ddcf115706fee5859f4ec101
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    Equals

---
+++ STRINGS/STRING/Equals.java
@@ -0,0 +1,12 @@
+package STRING;
+
+public class Equals {
+    public static void main(String[] args){
+        String s1="javatpoint";
+        String s2="javatpoint";
+        String s3="JAVATPOINT";
+        String s4="python";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+}}
