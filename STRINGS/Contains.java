afe1bbd1aa067d89f2e4ee08637efbde0cf7f79c
gopigollapudi <124708599+gopigollapudi@users.noreply.github.com>
2023-02-07 23:08:00 +0530

    Delete Contains.java

--- STRINGS/Contains.java
+++
@@ -1,8 +0,0 @@
-public class Contains {
-    public static void main(String[] args){
-        String name="what do you know about me";
-        System.out.println(name.contains("do you know"));
-        System.out.println(name.contains("about"));
-        System.out.println(name.contains("hello"));
-}}
-
