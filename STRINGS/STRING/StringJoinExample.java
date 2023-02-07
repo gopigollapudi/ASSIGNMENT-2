286feadfe39167dd8e726274ec3c28d14e6f1238
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    StringJoinExample

---
+++ STRINGS/STRING/StringJoinExample.java
@@ -0,0 +1,7 @@
+package STRING;
+
+public class StringJoinExample {
+    public static void main(String args[]){
+        String joinString1=String.join("-","welcome","to","javatpoint");
+        System.out.println(joinString1);
+    }}
