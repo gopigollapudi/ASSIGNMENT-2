fc3212227277890fa1a4b91c854e5c6a72773b1b
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    Endswith

---
+++ STRINGS/STRING/Endswith.java
@@ -0,0 +1,9 @@
+package STRING;
+
+public class Endswith {
+    public static void main(String[] args){
+        String s1="java by javatpoint";
+        System.out.println(s1.endsWith("t"));
+        System.out.println(s1.endsWith("point"));
+}}
+
