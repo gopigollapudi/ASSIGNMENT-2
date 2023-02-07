2c60e5dbbacdd085f6ad63c86c6f4d9880317133
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    StringToCharArrayExample

---
+++ STRINGS/STRING/StringToCharArrayExample.java
@@ -0,0 +1,10 @@
+package STRING;
+
+public class StringToCharArrayExample {
+    public static void main(String args[]){
+        String s1="hello";
+        char[] ch=s1.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            System.out.print(ch[i]);
+        }
+    }}
