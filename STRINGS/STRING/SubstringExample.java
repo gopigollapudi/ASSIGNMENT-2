89414ed2320efd4a32790c7dee768452ddf4a6d6
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    SubstringExample

---
+++ STRINGS/STRING/SubstringExample.java
@@ -0,0 +1,8 @@
+package STRING;
+
+public class SubstringExample {
+    public static void main(String args[]){
+        String s1="javatpoint";
+        System.out.println(s1.substring(2,4));//returns va
+        System.out.println(s1.substring(2));//returnsvatpoint
+    }}
