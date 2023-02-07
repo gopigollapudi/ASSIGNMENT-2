d14ee9b628ce2d76e0b4680fe249d27110bc243c
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    StringTrimExample

---
+++ STRINGS/STRING/StringTrimExample.java
@@ -0,0 +1,8 @@
+package STRING;
+
+public class StringTrimExample {
+    public static void main(String args[]){
+        String s1="  hello string   ";
+        System.out.println(s1+"javatpoint");//without trim()
+        System.out.println(s1.trim()+"javatpoint");//with trim()  
+    }}
