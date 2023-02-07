f5fab90a505305f612eb671856786e5cc0d69a21
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    StringValueOfExample

--- STRINGS/STRING/StringTrimExample.java
+++ STRINGS/STRING/StringTrimExample.java
@@ -4,5 +4,5 @@ public class StringTrimExample {
     public static void main(String args[]){
         String s1="  hello string   ";
         System.out.println(s1+"javatpoint");//without trim()
-        System.out.println(s1.trim()+"javatpoint");//with trim()  
+        System.out.println(s1.trim()+"javatpoint");//withtrim()
     }}
---
+++ STRINGS/STRING/StringValueOfExample.java
@@ -0,0 +1,10 @@
+package STRING;
+
+public class StringValueOfExample {
+    public static void main(String args[]) {
+        int value = 30;
+        String s1 = String.valueOf(value);
+        System.out.println(s1 + 10);//concatenating
+        // stringwith10
+    }
+}
