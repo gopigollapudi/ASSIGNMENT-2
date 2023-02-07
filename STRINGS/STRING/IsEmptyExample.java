1661fe3bb77846afbc739ad23ff3ceff1316c511
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    IsEmptyExample

---
+++ STRINGS/STRING/IsEmptyExample.java
@@ -0,0 +1,10 @@
+package STRING;
+
+public class IsEmptyExample {public static void main(String args[]){
+    String s1="";
+    String s2="javatpoint";
+
+    System.out.println(s1.isEmpty());
+    System.out.println(s2.isEmpty());
+}}
+
