f29c8c721a40c7f51bab1f5120b241a15968c3dc
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    ReplaceExample1

---
+++ STRINGS/STRING/ReplaceExample1.java
@@ -0,0 +1,8 @@
+package STRING;
+
+public class ReplaceExample1 {public static void main(String args[]){
+    String s1="javatpoint is a very good website";
+    String replaceString=s1.replace('a','e');//replaces all occurrences of 'a' to 'e'
+    System.out.println(replaceString);
+}}
+
