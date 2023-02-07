8acd49077393183dd68cc9bc5bf09c640eb1323f
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    LastIndexOfExample

---
+++ STRINGS/STRING/LastIndexOfExample.java
@@ -0,0 +1,9 @@
+package STRING;
+
+public class LastIndexOfExample {public static void main(String args[]){
+    String s1="this is index of example";//there are 2 's' characters in this sentence
+    int index1=s1.lastIndexOf('s');//returns last index of 's' char value
+    System.out.println(index1);//6
+}}
+
+
