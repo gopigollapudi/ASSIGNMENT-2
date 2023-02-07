e4165f38c7b573a09e8e6a89c9dcecf4dac8e98c
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    EqualsIgnorecase

---
+++ STRINGS/STRING/EqualsIgnorecase.java
@@ -0,0 +1,12 @@
+package STRING;
+
+public class EqualsIgnorecase {
+    public static void main(String[] args){
+        String s1="javatpoint";
+        String s2="javatpoint";
+        String s3="JAVATPOINT";
+        String s4="python";
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s1.equalsIgnoreCase(s4));
+}}
