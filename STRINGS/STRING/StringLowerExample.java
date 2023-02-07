e1beeeeed409311ee9fa99b7de7bef7358f19349
GOPI <gopigollapudi43@gmail.com>
2023-02-07 22:58:41 +0530

    StringLowerExample

---
+++ STRINGS/STRING/StringLowerExample.java
@@ -0,0 +1,9 @@
+package STRING;
+
+public class StringLowerExample {public static void main(String args[]){
+    String s1="JAVATPOINT HELLO stRIng";
+    String s1lower=s1.toLowerCase();
+    System.out.println(s1lower);
+}}
+
+
